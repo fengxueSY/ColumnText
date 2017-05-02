fa0ee20a996c5763be0405e7988f9ccc6b2f814e
666gps <yang@yang.local>
2017-05-02 10:42:03 +0800

    Initial Commit

---
+++ ColumnText/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColumnText
+//
+//  Created by 666gps on 2017/5/2.
+//  Copyright © 2017年 666gps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ColumnText/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColumnText
+//
+//  Created by 666gps on 2017/5/2.
+//  Copyright © 2017年 666gps. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
